46aed5248666d3bb754c2d1cce91d7759f418c38
4iar <4iar@users.noreply.github.com>
2015-12-28 16:05:36 +0000

    Add more data summary functions

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
 import sqlite3
 import json
+import time
+from statistics import mean, StatisticsError
 
 SQLITE_PATH = "db.sqlite"  # should make a config file
 LIST_OF_GYMS_FILE = "gyms.txt"
@@ -41,13 +43,16 @@ class Database:
             except sqlite3.OperationalError:
                 print("Database locked, trying again")
 
-    def get_data_as_dictlist(self, gym):
+    def get_raw_data_as_dictlist(self, gym):
         '''
         Return all observations for a given gym as a list. Each list item is a
         single observation containing the keys: date, day, time, num_people.
         '''
 
         # handle empty data?
+
+        if gym not in self.gyms:
+            return None
 
         operation = '''SELECT * FROM `{}` '''.format(gym)
 
@@ -62,45 +67,29 @@ class Database:
 
         return data_formatted
 
-    def get_data_as_json(self, gym):
-
+    def get_raw_data_as_json(self, gym):
         return json.dumps(self.get_data_as_dictlist(gym))
 
+    def get_hour_summary_data_as_json(self, gym):
 
+        raw_data = self.get_raw_data_as_dictlist(gym)
 
+        days = [d for d in range(1, 8)]
+        summary_data = {day: {hour: [] for hour in range(1, 25)} for day in days}
 
+        for observation in raw_data:
+            day = time.strptime(observation['day'], '%A').tm_wday + 1  # convert day to number (1-7)
+            hour = int(observation['time'].split(':')[0])  # strip the hour value from the string
+            summary_data[day][hour].append(observation['num_people'])
 
+        for day in summary_data:
+            for hour in summary_data[day]:
+                try:
+                    summary_data[day][hour] = mean(summary_data[day][hour])
+                except StatisticsError:  # if no data points
+                    summary_data[day][hour] = None
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return json.dumps(summary_data)
 
 
 
